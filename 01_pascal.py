d4309db045b61e6d99b9b1e6512dbf3f5e07b45c
Siddhant <post.siddhant@gmail.com>
2018-02-24 16:14:11 -0500

    implementing data loader and model function for training on pascal data

--- 01_pascal.py
+++ 01_pascal.py
@@ -12,36 +12,100 @@ from PIL import Image
 from functools import partial
 
 from eval import compute_map
-import models
+#import models
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
 CLASS_NAMES = [
-    'aeroplane',
-    'bicycle',
-    'bird',
-    'boat',
-    'bottle',
-    'bus',
-    'car',
-    'cat',
-    'chair',
-    'cow',
-    'diningtable',
-    'dog',
-    'horse',
-    'motorbike',
-    'person',
-    'pottedplant',
-    'sheep',
-    'sofa',
-    'train',
-    'tvmonitor',
+     'aeroplane',
+     'bicycle',
+     'bird',
+     'boat',
+     'bottle',
+     'bus',
+     'car',
+     'cat',
+     'chair',
+     'cow',
+     'diningtable',
+     'dog',
+     'horse',
+     'motorbike',
+     'person',
+     'pottedplant',
+     'sheep',
+     'sofa',
+     'train',
+     'tvmonitor',
 ]
 
 
 def cnn_model_fn(features, labels, mode, num_classes=20):
-    # Write this function
+
+    #Siddhanj: TODO Might want to take a random 224x224 crop at train and center 224X224 crop at test time
+    input_layer = tf.reshape(features["x"], [-1, 256, 256, 3])
+
+    # Convolutional Layer #1
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+
+    # Pooling Layer #1
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+    # Convolutional Layer #2 and Pooling Layer #2
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+    # Dense Layer
+    pool2_flat = tf.reshape(pool2, [-1, 64 * 64 * 64])
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024,
+                            activation=tf.nn.relu)
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+    # Logits Layer
+    logits = tf.layers.dense(inputs=dropout, units=num_classes)
+
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=logits, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.sigmoid(logits, name="softmax_tensor")
+    }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    #onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
+    loss = tf.identity(tf.losses.sigmoid_cross_entropy(
+        multi_class_labels=labels, logits=logits), name='loss')
+
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(
+            mode=mode, loss=loss, train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"])}
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
 def load_pascal(data_dir, split='train'):
@@ -58,7 +122,44 @@ def load_pascal(data_dir, split='train'):
             type np.int32, with 0s and 1s; 1s for classes that
             are active in that image.
     """
-    # Wrote this function
+
+    imageLabelDict = {}
+    num_images = sum(1 for line in open(data_dir + "/ImageSets/Main/" + split + ".txt"))
+    images = np.ndarray(shape=(num_images, 256, 256, 3), dtype=np.float32)
+    labels = np.ndarray(shape=(num_images, 20), dtype=np.int32)
+    weights = np.ndarray(shape=(num_images, 20), dtype=np.int32)
+    weights.fill(1)
+    i=0
+    for eachClass in CLASS_NAMES:
+        print(eachClass)
+        labelNumber = CLASS_NAMES.index(eachClass)
+        referenceFile = eachClass + "_" + split +".txt"
+
+        with open(data_dir+"/ImageSets/Main/"+referenceFile) as f:
+
+            for line in f:
+                words = line.split()
+
+
+                if(words[1] == '-1'):
+                    continue
+                if words[0] in imageLabelDict.keys():
+                    labels[imageLabelDict[words[0]], labelNumber] = 1
+                    weights[imageLabelDict[words[0]], labelNumber] = np.int32(words[0])
+                    continue
+
+                imageLabelDict[words[0]] = i
+                imageFileName = data_dir+"/JPEGImages/"+words[0]+".jpg"
+
+                rawImage = Image.open(imageFileName)
+                rawImage = rawImage.resize([256,256])
+                finalImageData = np.array(rawImage.getdata(),np.float32).reshape(rawImage.size[1], rawImage.size[0], 3)
+                images[i] = finalImageData
+                labels[i,labelNumber] =1
+                weights[i, labelNumber] = np.int32(words[0])
+                i=i+1
+
+    return images,labels, weights
 
 
 def parse_args():
@@ -82,6 +183,8 @@ def _get_el(arr, i):
 
 
 def main():
+    BATCH_SIZE = 100
+    NUM_ITERS = 10000
     args = parse_args()
     # Load training and eval data
     train_data, train_labels, train_weights = load_pascal(
