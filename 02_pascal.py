aadfa7bf230eeea47c6b3618c7998dd2ba4e10f1
Siddhant <post.siddhant@gmail.com>
2018-02-26 07:14:36 -0500

    changing logging folder for alexnet pascal

--- 02_pascal.py
+++ 02_pascal.py
@@ -267,7 +267,7 @@ def main():
     pascal_classifier = tf.estimator.Estimator(
         model_fn=partial(cnn_model_fn,
                          num_classes=train_labels.shape[1]),
-        model_dir="/tmp/pascal_model_scratch")
+        model_dir="/tmp/pascal_model_alexnet")
 
     tensors_to_log = {"loss": "loss"}
     logging_hook = tf.train.LoggingTensorHook(
